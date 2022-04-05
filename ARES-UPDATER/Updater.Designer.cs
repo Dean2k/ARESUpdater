fbcbf0d605ec0ab9e7bf0474e0c5a3242eed7591
Shrek <dean2k@live.co.uk>
2022-04-05 20:43:17 +0100

    New updater

---
+++ ARES-UPDATER/Updater.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ARES_UPDATER
+{
+    partial class Updater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updater));
+            this.cbVersions = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.downloadProgress = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPatchNotes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbVersions
+            // 
+            this.cbVersions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbVersions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVersions.FormattingEnabled = true;
+            this.cbVersions.Location = new System.Drawing.Point(81, 6);
+            this.cbVersions.Name = "cbVersions";
+            this.cbVersions.Size = new System.Drawing.Size(249, 28);
+            this.cbVersions.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Version";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(336, 6);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(92, 28);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update/Install";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // downloadProgress
+            // 
+            this.downloadProgress.Location = new System.Drawing.Point(16, 46);
+            this.downloadProgress.Name = "downloadProgress";
+            this.downloadProgress.Size = new System.Drawing.Size(508, 23);
+            this.downloadProgress.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(16, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(508, 25);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPatchNotes
+            // 
+            this.btnPatchNotes.Location = new System.Drawing.Point(434, 6);
+            this.btnPatchNotes.Name = "btnPatchNotes";
+            this.btnPatchNotes.Size = new System.Drawing.Size(92, 28);
+            this.btnPatchNotes.TabIndex = 5;
+            this.btnPatchNotes.Text = "Patch Notes";
+            this.btnPatchNotes.UseVisualStyleBackColor = true;
+            this.btnPatchNotes.Click += new System.EventHandler(this.btnPatchNotes_Click);
+            // 
+            // Updater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 99);
+            this.Controls.Add(this.btnPatchNotes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.downloadProgress);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbVersions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Updater";
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.Updater_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbVersions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ProgressBar downloadProgress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPatchNotes;
+    }
+}
+
---
+++ ARES-UPDATER/Updater.cs
@@ -0,0 +1,159 @@
+﻿using Octokit;
+using Octokit.Internal;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ARES_UPDATER
+{
+    public partial class Updater : Form
+    {
+        public IReadOnlyList<Release> Releases;
+        private static string fileLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static string guiLocation = fileLocation + @"\GUI\ARES.exe";
+
+        public Updater()
+        {
+            InitializeComponent();
+        }
+
+        private void Updater_Load(object sender, EventArgs e)
+        {
+            var connection = new Connection(new ProductHeaderValue("ARES"),
+            new HttpClientAdapter(() => HttpMessageHandlerFactory.CreateDefault()));
+
+            // and pass this connection to your client
+            var client = new GitHubClient(connection);
+            var releases = client.Repository.Release.GetAll("Dean2k", "A.R.E.S");
+            Releases = releases.Result;
+            foreach (var item in Releases)
+            {
+                cbVersions.Items.Add(item.Name);
+            }
+            cbVersions.SelectedIndex = 0;
+            string application = SHA256CheckSum(guiLocation);
+            string latestString = GetHashLatestAsync("https://raw.githubusercontent.com/Dean2k/A.R.E.S/main/VersionHashes/ARESGUI.txt");
+            if (application.ToLower() != latestString.ToLower())
+            {
+                MessageBox.Show("Your ARES version is currently not upto date, please update to the latest version");
+            }
+        }
+
+        private static void extractGUI()
+        {
+            try
+            {
+                using (Stream stream = File.OpenRead(fileLocation + @"\GUI.rar"))
+                {
+                    var reader = ReaderFactory.Open(stream);
+                    while (reader.MoveToNextEntry())
+                    {
+                        if (!reader.Entry.IsDirectory)
+                        {
+                            Console.WriteLine(reader.Entry.Key);
+                            reader.WriteEntryToDirectory(fileLocation + @"\GUI", new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Extraction Error: " + ex.Message);
+            }
+
+        }
+
+        private static void startARES()
+        {
+            try
+            {
+                string commands = string.Format("/C ARES.exe");
+
+                Process p = new Process();
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "CMD.EXE",
+                    Arguments = commands,
+                    WorkingDirectory = fileLocation + @"\GUI\",
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden
+                };
+                p.StartInfo = psi;
+                p.Start();
+            }
+            catch { }
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            label2.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+            downloadProgress.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string releaseVersion = Releases.FirstOrDefault(x => x.Name == cbVersions.Text).Assets.FirstOrDefault(x => x.Name == "GUI.rar").BrowserDownloadUrl;
+            if (releaseVersion != default)
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri(releaseVersion), fileLocation + @"\GUI.rar");
+            }
+
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            label2.Text = "Completed";
+            extractGUI();
+            startARES();
+        }
+
+
+        private static string SHA256CheckSum(string filePath)
+        {
+            try
+            {
+                using (SHA256 SHA256 = SHA256Managed.Create())
+                {
+                    using (FileStream fileStream = File.OpenRead(filePath))
+                        return BitConverter.ToString(SHA256.ComputeHash(fileStream)).Replace("-", "");
+                }
+            }
+            catch { return "0"; }
+        }
+
+        private static string GetHashLatestAsync(string url)
+        {
+            string result;
+            using (HttpClient client = new HttpClient())
+            {
+                result = client.GetStringAsync(url).Result;
+            }
+            return result;
+        }
+
+        private void btnPatchNotes_Click(object sender, EventArgs e)
+        {
+            string patchNotes = Releases.FirstOrDefault(x => x.Name == cbVersions.Text).Body;
+            MessageBox.Show(patchNotes);
+        }
+    }
+}
